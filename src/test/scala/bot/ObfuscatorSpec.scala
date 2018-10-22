efed83a7c6b0de7f55b4c0e0f94b55901e900760
datadonK23 <datadonk23@gmail.com>
2018-10-22 11:40:40 +0200

    ObfuscateToken tests

--- src/test/scala/bot/ObfuscatorSpec.scala
+++ src/test/scala/bot/ObfuscatorSpec.scala
@@ -3,11 +3,19 @@ package bot
 import org.scalatest._
 
 class ObfuscatorSpec extends FlatSpec with Matchers {
-  "Obfuscator.obfuscate" should "strip first word from input string" in {
+  "Obfuscator.obfuscateText" should "strip first word from input string" in {
     Obfuscator.obfuscateText("Test1 Test Test").getOrElse("").split(" ").length shouldEqual 2
   }
 
   it should "not return input text exactly" in {
     Obfuscator.obfuscateText("Test2 Test Test").getOrElse("") should not equal "Test Test"
+  }
+
+  "Obfuscator.obfuscateToken" should "obfuscate all characters in string" in {
+    Obfuscator.obfuscateToken("j", 1.0) should (be ("J") or be ("_|"))
+  }
+
+  it should "not obfuscate character when obfuscationIndex is 0" in {
+    Obfuscator.obfuscateToken("t", 0.0) should be ("t")
   }
 }
